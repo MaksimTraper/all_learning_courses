836573a9a5fd261f088f38430307b26f87881056
MaksimTraper <94693148+MaksimTraper@users.noreply.github.com>
2023-11-24 00:52:43 +0300

    Delete University/University/Compiler Yacc directory

--- University/University/Compiler Yacc/Compiler/Exceptions/TypesMismatchException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Compiler.Exceptions 
-{
-    public class TypesMismatchException : MyException
-    {
-        public TypesMismatchException(string message = "") : base(
-            "Несоответствие типов. " + message, new MyErrorExceptionType())
-        { }
-    }
-}
--- University/University/Compiler Yacc/Compiler/Nodes/MyType.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Compiler.Nodes
-{
-    /// <summary>
-    /// Типы данных 
-    /// </summary>
-    internal enum MyType { Bool, Double, None, String }
-}
